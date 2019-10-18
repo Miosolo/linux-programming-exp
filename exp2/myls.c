e4f667e3c1a3a1b5bd7f25bf2f9a098906be3f07
Miosolo <xht2012.jn@gmail.com>
2019-10-18 20:48:18 +0800

    exp3 finished

--- exp2/myls.c
+++ exp2/myls.c
@@ -15,55 +15,60 @@
 
 #define MAX_TASKS 10
 
-char* get_perm(mode_t m) {
+char *get_perm(mode_t m) {
   int i;
   unsigned int mask = 0x7;
-  char *result = (char*)malloc(10);
+  char *result = (char *)malloc(10);
 
   static char *perm[] = {"---", "--x", "-w-", "-wx",
                          "r--", "r-x", "rw-", "rwx"};
   for (i = 3; i > 0; i--) {
-    sprintf(result[3-i], "%3s", perm[(m >> (i - 1) * 3) & mask]);
+    sprintf(&result[9 - 3 * i], "%3s", perm[(m >> (i - 1) * 3) & mask]);
   }
   return result;
 }
 
 char get_type(mode_t m) {
   if (S_ISDIR(m)) {
-      return('d');
-    } else if (S_ISLNK(m)) {
-      return('l');
-    } else {
-      return('-');
-    }
+    return ('d');
+  } else if (S_ISLNK(m)) {
+    return ('l');
+  } else {
+    return ('-');
+  }
 }
 
-bool ls(char *path, bool showdir) {
+void ls(char *path, bool showdir) {
   DIR *cwd = opendir(path);
   if (cwd == NULL) {
     perror("error: cannot open directory: ");
-    return false;
+    return;
+  }
+  if (showdir) {
+    printf("%s:\n", path);
   }
 
   // iter through all files
+  char timebuf[255];
+  char dirbuf[1024];
   struct dirent *entry;
   while ((entry = readdir(cwd)) != NULL) {
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+      continue;  // omit . and ..
+
+    sprintf(dirbuf, "%s/%s", path, entry->d_name);
     struct stat curstat;
-    if ((lstat(strcat(cwd, entry->d_name), &curstat)) != 0) {
+    if ((lstat(dirbuf, &curstat)) != 0) {
       // fail
       perror("error: cannot get file attributes: ");
       continue;
     }
 
-    printf("%c%s %d %10s %10s %7d %s %s\n", 
-           get_type(curstat.st_mode),
-           get_type(curstat.st_mode),
-           curstat.st_nlink,
-           getpwuid(curstat.st_uid), 
-           getgrgid(curstat.st_gid), 
-           curstat.st_size, 
-           localtime(curstat.st_ctime),
-           entry->d_name);
+    strftime(timebuf, 255, "%Y-%m-%d %H:%M:%S", localtime(&(curstat.st_ctime)));
+    printf("%c%s %2d %9s %9s %7d %s %s\n", get_type(curstat.st_mode),
+           get_perm(curstat.st_mode), curstat.st_nlink,
+           getpwuid(curstat.st_uid)->pw_name, getgrgid(curstat.st_gid)->gr_name,
+           curstat.st_size, timebuf, entry->d_name);
   }
 }
 
@@ -71,6 +76,8 @@ int main(int argc, char *argv[]) {
   // input param filling
   if (argc == 1) {
     ls(".", false);
+  } else if (argc == 2) {
+    ls(argv[1], false);
   } else {
     for (int i = 1; i < argc; i++) {
       ls(argv[i], true);
